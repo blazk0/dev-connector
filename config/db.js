23d90035e6995ef1949f7af31648f29682d2e40e
Amr Abo Elenin <amr_aboelenin95@yahoo.com>
2019-05-14 16:50:44 +0200

    finshed profile api route

--- config/db.js
+++ config/db.js
@@ -4,14 +4,18 @@ const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, { useNewUrlParser: true });
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    });
 
     console.log('MongoDB Connected');
-  } catch(err) {
+  } catch (err) {
     console.error(err.message);
     // Exit Process with failure
     process.exit(1);
   }
-}
+};
 
 module.exports = connectDB;
